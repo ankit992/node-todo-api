8fc4d0b05953b352a8efe3297ad4ed8114ce39ba
Ankit Arora <ankit992@live.in>
2017-11-01 01:48:35 +0530

    Object id in ongodb extracting timestamp from it

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,12 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+// var user = {name: 'Ankit', age: 25};
+// var {name} = user;
+// console.log(name)
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if(err) {
@@ -16,16 +24,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Ankit',
-        age: 25,
-        location: 'Gurugram'
-    }, (err, result) => {
-        if(err){
-            return console.log('Unable to insert User', err);
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    // db.collection('Users').insertOne({
+    //     name: 'Ankit',
+    //     age: 25,
+    //     location: 'Gurugram'
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log('Unable to insert User', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+    // });
 
     db.close();
 });
