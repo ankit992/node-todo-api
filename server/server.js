807403fb930dee754c070850644921fe7fdd8a27
Ankit Arora <ankit992@live.in>
2017-11-03 00:06:57 +0530

    Add post todos route

--- server/server.js
+++ server/server.js
@@ -4,3 +4,25 @@ var bodyParser = require('body-parser');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/Todo');
 var { User } = require('./models/User');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc)=> {
+        res.status(201).send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+// Get /Todos/123/123/123
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
